06fd488633d3ca39b2f85139a96c0d0128097a83
zaki maliki <malikizakitop@gmail.com>
2023-08-06 02:14:31 +0700

    first commit

--- app/index.js
+++ app/index.js
@@ -1,12 +1,15 @@
 import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+// external
+import styles from "./styles"
 
 export default function Page() {
   return (
     <View style={styles.container}>
       <View style={styles.main}>
         <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+        {/* inline */}
+        <Text style={{fontSize: 36,color: "#38434D"}}>This is the first page of your app</Text>
         <Link href="/profile">Open Profile</Link>
         <Link href="/profile?name=Zaki&age=28">Zaki's Profile</Link>
         <Link href={{pathname:'profile',params:{name:'maliki',age:28}}}>Maliki's Profile</Link>
@@ -17,24 +20,25 @@ export default function Page() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
+// style scoped
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     padding: 24,
+//   },
+//   main: {
+//     flex: 1,
+//     justifyContent: "center",
+//     maxWidth: 960,
+//     marginHorizontal: "auto",
+//   },
+//   title: {
+//     fontSize: 64,
+//     fontWeight: "bold",
+//   },
+//   subtitle: {
+//     fontSize: 36,
+//     color: "#38434D",
+//   },
+// });
